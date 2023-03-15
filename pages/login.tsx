3a165df8efa2eb2be5bf47cd05132ed037bce82e
Aisamuddin <aisam0094@gmail.com>
2023-03-15 21:38:11 +0800

    change login/register ui

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,23 +1,28 @@
-import React, { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import { FormEvent, useState } from "react";
 import Link from "next/link";
 import { signInWithEmailAndPassword } from "@firebase/auth";
 import { auth } from "../firebase";
 import { useRouter } from "next/router";
-import {
-  NotificationContainer,
-  NotificationManager,
-} from "react-notifications";
 import { NextRouter } from "next/router";
+import {
+  Box,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+  InputRightElement,
+  InputGroup,
+  useToast,
+} from "@chakra-ui/react";
 
 const Login = () => {
   const router: NextRouter = useRouter();
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const { email, password } = formData;
+  const toast = useToast();
 
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
@@ -33,123 +38,118 @@ const Login = () => {
         const errorCode = error.code;
         const errorMessage = error.message;
         if (errorCode === "auth/too-many-requests") {
-          createNotification(
+          createErrorNotification(
             "Access to this account has been temporarily disabled due to many failed login attempts"
           );
         } else if (
           errorCode === "auth/wrong-password" ||
           errorCode === "auth/user-not-found"
         ) {
-          createNotification("Email or password is wrong");
+          createErrorNotification("Email or password is wrong");
         } else {
-          createNotification("Error signing in");
+          createErrorNotification("Error signing in");
         }
         console.log({ errorCode });
         console.log({ errorMessage });
       });
   };
 
-  const change = (event: { target: HTMLInputElement }): void => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
+  function handleEmailInput(e: { target: HTMLInputElement }) {
+    setEmail(e.target.value);
+  }
+
+  function handlePasswordInput(e: { target: HTMLInputElement }) {
+    setPassword(e.target.value);
+  }
 
   const submit = (event: FormEvent): void => {
     event.preventDefault();
     signIn(email, password);
   };
 
-  const createNotification = (errorMsg: string): any => {
-    return NotificationManager.error(errorMsg, "", 500);
+  const createErrorNotification = (errorMsg: string): any => {
+    toast({
+      title: "Error",
+      description: errorMsg,
+      status: "error",
+      duration: 1000,
+      isClosable: true,
+    });
   };
 
+  function isEmailError() {
+    if (email === "") return true;
+  }
+
+  function isPasswordError() {
+    if (password === "") return true;
+  }
+
   return (
-    <main className="login-main">
-      <NotificationContainer />
-      <div className="form-container">
-        <h1 className="form-secondary-title">Sign in to your account</h1>
+    <div className="login-main mt-4">
+      <Box className="form-container">
+        <h1 className="form-secondary-title text-primary font-bold text-xl">
+          Sign in to your account
+        </h1>
         <form className="form-inputs" onSubmit={(event) => submit(event)}>
-          {/* Email */}
-          <input
-            type="email"
-            name="email"
-            className="form-field"
-            placeholder="Email"
-            value={email}
-            onChange={(event) => change(event)}
-            required
-          />
-          {/* Password wrapper */}
-          <div className="password-wrapper">
-            {/* Password */}
-            <input
-              type={isPasswordVisible ? "text" : "password"}
-              name="password"
-              className="form-field"
-              placeholder="Password"
-              value={password}
-              onChange={(event) => change(event)}
-              required
+          <FormControl isRequired>
+            {/* Email */}
+            <Input
+              type="email"
+              value={email}
+              onChange={handleEmailInput}
+              placeholder="Email Address"
             />
-            {/* Eye indicator */}
-            <div
-              className="eye-indicator eye"
-              onClick={togglePasswordVisibility}
-            >
-              {isPasswordVisible ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
-                    clipRule="evenodd"
-                  />
-                  <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
-                </svg>
+            {!isEmailError ? (
+              <FormHelperText>Enter your email.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Email is required.</FormErrorMessage>
+            )}
+            {/* Password */}
+            <InputGroup>
+              <Input
+                type={isPasswordVisible ? "text" : "password"}
+                value={password}
+                onChange={handlePasswordInput}
+                placeholder="Password"
+              />
+              {/* Password visibility indicator */}
+              <InputRightElement
+                className="mt-2"
+                onClick={togglePasswordVisibility}
+              >
+                {isPasswordVisible ? (
+                  <img className="w-5 h-5 opacity-50" src="/eye_on.svg" />
+                ) : (
+                  <img className="w-5 h-5 opacity-50" src="/eye_off.svg" />
+                )}
+              </InputRightElement>
+              {!isPasswordError ? (
+                <FormHelperText>Enter your password.</FormHelperText>
               ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <FormErrorMessage>Password is required.</FormErrorMessage>
               )}
-            </div>
-          </div>
+            </InputGroup>
+          </FormControl>
+
           {/* Submit button */}
-          <input
-            type="submit"
-            className="btn btn-red form-submit"
-            value="LOG IN"
-          />
+          <Button colorScheme="red" type="submit">
+            LOGIN
+          </Button>
         </form>
         <div className="form-notice-container">
-          <div>
-            <input className="" name="remember-me" type="checkbox" />
-            <label htmlFor="remember-me" className="form-notice">
-              Remember me
-            </label>
-          </div>
-          <Link href="#">
-            <a className="form-notice">Forgot password?</a>
-          </Link>
           <h3 className="form-notice">
             Don't have an account?
-            <Link href="/register"> Register here.</Link>
+            <Link href="/register">
+              <span className="text-primary cursor-pointer">
+                {" "}
+                Register here.
+              </span>
+            </Link>
           </h3>
         </div>
-      </div>
-    </main>
+      </Box>
+    </div>
   );
 };
 
--- pages/register.tsx
+++ pages/register.tsx
@@ -4,22 +4,25 @@ import { createUserWithEmailAndPassword } from "@firebase/auth";
 import { auth } from "../firebase";
 import { NextRouter, useRouter } from "next/router";
 import {
-  NotificationContainer,
-  NotificationManager,
-} from "react-notifications";
+  Box,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+  InputRightElement,
+  InputGroup,
+  useToast,
+} from "@chakra-ui/react";
 
 const Register = () => {
   const router: NextRouter = useRouter();
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [isPassword2Visible, setIsPassword2Visible] = useState(false);
-  const [formData, setFormData] = useState({
-    // username: "",
-    email: "",
-    password: "",
-    password2: "",
-  });
-
-  const { email, password, password2 } = formData;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const toast = useToast();
 
   const createUser = (email: string, password: string) => {
     createUserWithEmailAndPassword(auth, email, password)
@@ -30,7 +33,7 @@ const Register = () => {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        createNotification(errorCode);
+        createErrorNotification(errorCode);
         console.log({ errorCode });
         console.log({ errorMessage });
       });
@@ -44,166 +47,176 @@ const Register = () => {
     setIsPassword2Visible(!isPassword2Visible);
   };
 
-  const change = (event: { target: HTMLInputElement }) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
-
   const submit = (event: FormEvent) => {
     event.preventDefault();
     if (password === password2) {
       createUser(email, password);
     } else {
-      createNotification("password not matching");
+      createErrorNotification("password not matching");
     }
   };
 
-  const createNotification = (errorMsg: string) => {
+  function handleEmailInput(e: { target: HTMLInputElement }) {
+    setEmail(e.target.value);
+  }
+
+  function handlePasswordInput(e: { target: HTMLInputElement }) {
+    setPassword(e.target.value);
+  }
+
+  function handlePassword2Input(e: { target: HTMLInputElement }) {
+    setPassword2(e.target.value);
+  }
+
+  const createErrorNotification = (errorMsg: string) => {
     switch (errorMsg) {
       case "auth/weak-password":
-        return NotificationManager.error(
-          "Password should be at least 6 characters"
-        );
+        toast({
+          title: "Error",
+          description: "Password should be at least 6 characters",
+          status: "error",
+          duration: 1000,
+          isClosable: true,
+        });
         break;
       case "auth/invalid-email":
-        return NotificationManager.error("Invalid email format", "", 500);
+        toast({
+          title: "Error",
+          description: "Invalid email format",
+          status: "error",
+          duration: 1000,
+          isClosable: true,
+        });
         break;
       case "auth/email-already-in-use":
-        return NotificationManager.error("Email already in use", "", 500);
+        toast({
+          title: "Error",
+          description: "Email already in use",
+          status: "error",
+          duration: 1000,
+          isClosable: true,
+        });
         break;
       case "password not matching":
-        return NotificationManager.error("Password not matching", "", 500);
+        toast({
+          title: "Error",
+          description: "Password not matching",
+          status: "error",
+          duration: 1000,
+          isClosable: true,
+        });
         break;
       default:
-        return NotificationManager.error("Error registering account", "", 500);
+        toast({
+          title: "Error",
+          description: "Error registering account",
+          status: "error",
+          duration: 1000,
+          isClosable: true,
+        });
     }
   };
 
+  function isEmailError() {
+    if (email === "") return true;
+  }
+
+  function isPasswordError() {
+    if (password === "") return true;
+  }
+
   return (
-    <main className="login-main">
-      <NotificationContainer />
-      <div className="form-container">
-        <h1 className="form-secondary-title">Register your account</h1>
+    <div className="login-main mt-4">
+      <Box className="form-container">
+        <h1 className="form-secondary-title text-primary font-bold text-xl">
+          Register your account
+        </h1>
+
         <form className="form-inputs" onSubmit={(event) => submit(event)}>
-          {/* Email */}
-          <input
-            type="text"
-            name="email"
-            className="form-field"
-            placeholder="Email Address"
-            value={email}
-            onChange={(event) => change(event)}
-            required
-          />
-          {/* Password wrapper */}
-          <div className="password-wrapper">
-            {/* Password */}
-            <input
-              type={isPasswordVisible ? "text" : "password"}
-              name="password"
-              className="form-field"
-              placeholder="Password"
-              value={password}
-              onChange={(event) => change(event)}
+          <FormControl isRequired>
+            {/* Email */}
+            <Input
+              type="text"
+              name="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={handleEmailInput}
               required
             />
-            {/* Password visibility indicator */}
-            <div
-              className="eye-indicator eye"
-              onClick={togglePasswordVisibility}
-            >
-              {isPasswordVisible ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
-                    clipRule="evenodd"
-                  />
-                  <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
-                </svg>
+            {!isEmailError ? (
+              <FormHelperText>Enter your email.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Email is required.</FormErrorMessage>
+            )}
+            {/* Password wrapper */}
+            <InputGroup>
+              {/* Password */}
+              <Input
+                type={isPasswordVisible ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordInput}
+                required
+              />
+              {/* Password visibility indicator */}
+              <InputRightElement
+                className="mt-2"
+                onClick={togglePasswordVisibility}
+              >
+                {isPasswordVisible ? (
+                  <img className="w-5 h-5 opacity-50" src="/eye_on.svg" />
+                ) : (
+                  <img className="w-5 h-5 opacity-50" src="/eye_off.svg" />
+                )}
+              </InputRightElement>
+            </InputGroup>
+
+            {/* Password2 wrapper */}
+            <InputGroup>
+              {/* Password2 */}
+              <Input
+                type={isPassword2Visible ? "text" : "password"}
+                name="password2"
+                className="form-field"
+                placeholder="Reenter Password"
+                value={password2}
+                onChange={handlePassword2Input}
+                required
+              />
+              {/* Password2 visibility indicator */}
+              <InputRightElement
+                className="mt-2"
+                onClick={togglePassword2Visibility}
+              >
+                {isPassword2Visible ? (
+                  <img className="w-5 h-5 opacity-50" src="/eye_on.svg" />
+                ) : (
+                  <img className="w-5 h-5 opacity-50" src="/eye_off.svg" />
+                )}
+              </InputRightElement>
+              {!isPasswordError ? (
+                <FormHelperText>Enter your password.</FormHelperText>
               ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <FormErrorMessage>Password is required.</FormErrorMessage>
               )}
-            </div>
-          </div>
-          {/* Password2 wrapper */}
-          <div className="password-wrapper">
-            {/* Password2 */}
-            <input
-              type={isPassword2Visible ? "text" : "password"}
-              name="password2"
-              className="form-field"
-              placeholder="Reenter Password"
-              value={password2}
-              onChange={(event) => change(event)}
-              required
-            />
-            {/* Password2 visibility indicator */}
-            <div
-              className="eye-indicator eye"
-              onClick={togglePassword2Visibility}
-            >
-              {isPassword2Visible ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
-                    clipRule="evenodd"
-                  />
-                  <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              )}
-            </div>
-          </div>
+            </InputGroup>
+          </FormControl>
           {/* Submit button */}
-          <input
-            type="submit"
-            className="btn btn-red form-submit"
-            value="CREATE ACCOUNT"
-          />
+          <Button colorScheme="red" type="submit">
+            CREATE ACCOUNT
+          </Button>
         </form>
         <div className="form-notice-container">
           <h3>
-            Already have an account?<Link href="/login"> Login here</Link>
+            Already have an account?
+            <Link href="/login">
+              <span className="text-primary cursor-pointer"> Login here</span>
+            </Link>
           </h3>
         </div>
-      </div>
-    </main>
+      </Box>
+    </div>
   );
 };
 
