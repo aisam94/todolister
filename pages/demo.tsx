7b2da0069976374b24adff52d1b71e74d6bec4bb
Aisamuddin <aisam0094@gmail.com>
2023-03-24 18:36:06 +0800

    add default todo for demo

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -3,9 +3,38 @@ import Todo from "../components/Todo";
 import type { NextPage } from "next";
 
 const Demo = (): JSX.Element => {
+  const description =
+    "This is an example of a todo item description. You can edit the details for due dates, priority, tags and description using this section.";
+  const demoTodo = [
+    {
+      id: "1",
+      text: "Start by adding your own todo",
+      checked: false,
+      description: description,
+      priority: "high",
+      tags: ["demo", "test", "first"],
+    },
+    {
+      id: "2",
+      text: "Click todo item to edit the details",
+      checked: false,
+      description: description,
+      priority: "low",
+      tags: ["demo", "test", "second"],
+    },
+    {
+      id: "3",
+      text: "This is a checked todo item",
+      checked: true,
+      description: description,
+      priority: "low",
+      tags: ["demo", "test", "third"],
+    },
+  ];
+
   return (
     <div className="w-full flex justify-center">
-      <Todo todoData={[]} />
+      <Todo todoData={demoTodo} />
     </div>
   );
 };
